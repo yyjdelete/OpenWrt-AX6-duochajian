96eb74834eec6f195c4bc118b40f11c27a753ebd
jingleijack <tb6750217@gmail.com>
2021-07-18 17:22:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -50,3 +50,4 @@ git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 #添加smartdns
 git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng.git package/luci-app-chinadns-ng
